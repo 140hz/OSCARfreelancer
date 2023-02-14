ff72ccc4316b84800effc9a13890b522a6b33f77
140hz <oscar_king62@hotmail.com>
2023-02-13 23:24:05 -0600

    update audio- still need to fix audio autoplay

--- js/app.js
+++ js/app.js
@@ -1,16 +1,7 @@
 var audio=document.getElementById("audio");
 
-    var boton = document.getElementById("boton");  
-        document.getElementById("pause").addEventListener("click", function() {
-            // hacemos pausa
-            audio.pause();
-        }); 
- 
-		document.getElementById("play").addEventListener("click", function() {
-			// Si deseamos que inicie siempre desde el principio
-			//audioElement.currentTime = 0;
- 
-			// iniciamos el audio
+    var boton = document.getElementById("STFU");  
+
+		boton.onclick = function() {
 			audio.play();
-		});
-       
+		  };
